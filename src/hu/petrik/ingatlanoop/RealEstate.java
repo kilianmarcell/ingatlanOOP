ccca3ddc2db59ebe15f4de09202b7c161c820182
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-02-16 15:12:48 +0100

    Ingatlan létrehozva

--- src/hu/petrik/ingatlanoop/RealEstate.java
+++ src/hu/petrik/ingatlanoop/RealEstate.java
@@ -16,4 +16,19 @@ public class RealEstate {
     private double latitude;
     private double longitude;
 
+    private RealEstate(String s) {
+        String[] dates = s.split(",");
+        this.street = dates[0];
+        this.city = dates[1];
+        this.zip = Integer.parseInt(dates[2]);
+        this.state = dates[3];
+        this.beds = Integer.parseInt(dates[4]);
+        this.baths = Integer.parseInt(dates[5]);
+        this.sqFt = Integer.parseInt(dates[6]);
+        this.type = dates[7];
+        //this.sale_date = dates[8];
+        this.price = Integer.parseInt(dates[9]);
+        this.longitude = Double.parseDouble(dates[10].replace('.', ','));
+        this.latitude = Double.parseDouble(dates[11].replace('.', ','));
+    }
 }
---
+++ src/hu/petrik/ingatlanoop/RealEstates.java
@@ -0,0 +1,4 @@
+package hu.petrik.ingatlanoop;
+
+public class RealEstates {
+}
